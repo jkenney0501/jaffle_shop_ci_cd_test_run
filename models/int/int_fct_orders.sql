39e27f111262ca6e947d0b1234564f6125081fc6
James Kenney <james.kenney@astricadata.com>
2025-08-08 19:47:55 +0000

    dev is built out

---
+++ models/int/int_fct_orders.sql
@@ -0,0 +1,48 @@
+-- amount comes from payments
+-- use order_id, customer_id, amount
+-- materialize as incremental
+
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'order_id',
+        incremental_strategy='merge'
+    )
+}}
+
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
+
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+
+select * 
+from final
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where order_date >= (select max(order_date) from {{ this }}) 
+{% endif %}
